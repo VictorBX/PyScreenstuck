1e03ba45a8f947a2c744501191a3d47a11e7e8b2
Victor Barrera <victornbarrera@yahoo.com>
2013-01-12 19:58:08 -0800

    Setup

---
+++ README.txt
@@ -0,0 +1,21 @@
+PyScreenstuck
+===========
+
+Introduction
+------------
+PyScreenstuck is a Python script that checks if there is a new update on the webcomic Homestuck, created by Andrew Hussie. Is is very similar to my other project Screenstuck except for one minor difference: Instead of an image showing up on your desktop screen, a text message is sent to your phone via Google Voice. 
+
+Requires
+--------
+- Google Voice Account
+- pygooglevoice
+
+Progress
+--------
+- Checks every minute for an update
+
+Todo
+----
+- Finish implementing pygooglevoice
+- Send text msg
+- User input on time delay between checking for updates
---
+++ urltext.py
@@ -0,0 +1,41 @@
+import os
+import time
+import urllib
+import time
+
+#Website RSS location
+w_location = "http://www.mspaintadventures.com/rss/rss.xml"
+w_rss = urllib.urlopen(w_location).read()
+
+#Check if file exists
+if os.path.isfile("savedate.txt") == 0:
+	upd_loc = w_rss.find(";p=")
+	upd_num = w_rss[upd_loc+3:upd_loc+9]
+	file_open = open("savedate.txt", "w")
+	file_open.write(upd_num)
+	file_open.close()
+
+#Loop and check every minute
+while 1:
+	w_rss_new = urllib.urlopen(w_location).read()
+	updl = w_rss_new.find(";p=")
+	updn = w_rss_new[updl+3:updl+9]
+	print updn
+	fopen = open("savedate.txt","r")
+	check = fopen.readline()
+	fopen.close()
+	print check
+	
+	#Compare
+	if check != updn:
+		print "New Update!"
+		fopen_new = open("savedate.txt","w")
+		fopen_new.write(updn)
+		fopen_new.close()
+	else:
+		print "No New Update"
+	
+	#Sleep	
+	time.sleep(60)
+
+
